928483bdfee0e1cd59259f32477d8d8fd6f65234
NavasKoya <65574017+NavasKoya@users.noreply.github.com>
2021-06-22 00:29:11 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Booking Cart',
+      title: 'Book Cart',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
